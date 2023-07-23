29d5209da5cb3bebf6376e6324986cddd45d6195
roshanleharwani <roshanleharwani@gmail.com>
2023-07-23 15:52:04 +0530

    final commit

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-import requests
+import webbrowser
 
-headers = {
-    "Content-type": "application/json",
-    "Authorization": "Bearer HDZXieZcFnLtkNHk",
-}
-id = [88390, 87855, 87854]
-for i in id:
-    response = requests.delete(f"https://sizl.ink/api/url/{i}/delete", headers=headers)
-    print(response)
+webbrowser.open("google.com")
